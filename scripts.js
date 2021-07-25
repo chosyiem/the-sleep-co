19c873529d0252c3e31b6691139f03f380a8b706
chosyiem <85787525+chosyiem@users.noreply.github.com>
2021-07-25 17:05:29 +0530

    Add captions

--- scripts.js
+++ scripts.js
@@ -18,3 +18,8 @@ lightGallery(document.getElementById('lightgallery'), {
   speed: 500,
   download: false
 });
+lightGallery(document.getElementById('lightgallery-2'), {
+  speed: 500,
+  download: false,
+  getCaptionFromTitleOrAlt: true
+});
